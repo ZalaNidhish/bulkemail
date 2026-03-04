972f0b42a482b7e2feb5c8d6f4d995708477021f
Nidhish Zala <nidhish1132007@gmail.com>
2026-03-04 19:39:37 +0530

    test4

--- server/index.js
+++ server/index.js
@@ -230,4 +230,6 @@ app.post("/api/send-bulk", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
